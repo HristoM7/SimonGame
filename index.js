c52329447527df056add046c936ca8800c9fa3dc
HristoM7 <136826912+HristoM7@users.noreply.github.com>
2024-10-21 23:09:09 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,82 @@
+var buttonColours = ["red", "blue", "green", "yellow"];
+var gamePattern = [];
+var userClickedPattern = [];
+var level = 0;
+var started = false;
+
+$(document).ready(function() {
+  $("#level-title").text("Level " + level);
+  nextSequence();
+  started = true;
+});
+
+$(".btn").click(function() {
+  var userChosenColour = $(this).attr("id");
+  userClickedPattern.push(userChosenColour);
+
+  animatePress(userChosenColour);
+  playSound(userChosenColour);
+  checkAnswer(userClickedPattern.length-1);  
+});
+
+function nextSequence() {
+  userClickedPattern = [];
+  level++;
+  $("#level-title").text("Level " + level);
+  
+  var randomNumber = Math.floor(Math.random() * 4);
+  var randomChosenColour = buttonColours[randomNumber];
+  gamePattern.push(randomChosenColour);
+
+  $("#" + randomChosenColour).fadeIn(100).fadeOut(100).fadeIn(100);
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  var audio = new Audio("sounds/" + name + ".mp3");
+  audio.play();
+}
+
+function animatePress(currentColor) {  
+  $("#" + currentColor).addClass("pressed");
+
+  setTimeout(function () {
+    $("#" + currentColor).removeClass("pressed");
+  }, 100);
+}
+
+function checkAnswer(currentLevel) {
+  if (gamePattern[currentLevel] === userClickedPattern[currentLevel]) {
+    console.log("success");
+
+    if (userClickedPattern.length === gamePattern.length){
+      setTimeout(function () {
+        nextSequence();
+      }, 1000);
+    }
+  } else {
+    var audioWrong = new Audio("sounds/wrong.mp3");
+    audioWrong.play();
+    $("body").addClass("game-over");
+    setTimeout(function(){
+      $("body").removeClass("game-over");
+    }, 200);
+    
+    $("#level-title").text("Game Over, Restarting...");
+
+    // Рестартираме играта след 2 секунди
+    setTimeout(function(){
+      startOver();
+    }, 2000);
+  }
+}
+
+function startOver(){
+  level = 0;
+  gamePattern = [];
+  $("#level-title").text("Level " + level);
+  
+  setTimeout(function() {
+    nextSequence();
+  }, 1000); // Започва играта отново след 1 секунда пауза
+}
